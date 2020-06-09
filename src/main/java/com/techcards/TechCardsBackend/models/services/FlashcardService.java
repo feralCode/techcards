b3d02fa0a1083ffaa3fad4c5596550aa9e2d8131
Will Rowston <willrowston@gmail.com>
2020-06-09 13:32:11 -0400

    added CRUD methods to flashcardService

--- src/main/java/com/techcards/TechCardsBackend/models/services/FlashcardService.java
+++ src/main/java/com/techcards/TechCardsBackend/models/services/FlashcardService.java
@@ -1,9 +1,11 @@
 package com.techcards.TechCardsBackend.models.services;
 
 import com.techcards.TechCardsBackend.models.dao.flashcards.Flashcard;
+import com.techcards.TechCardsBackend.models.dao.flashcards.Flashcard;
 import com.techcards.TechCardsBackend.models.dao.flashcards.FlashcardDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -18,12 +20,23 @@ public class FlashcardService {
     public Flashcard getFlashcardById(UUID id) {
         Flashcard flashcard = new Flashcard();
 
-        flashcard.setId(id);
-        flashcard.setClue("This is a CLUE");
-        flashcard.setAnswer("This is an ANSWER");
-
-        System.out.println(flashcard.getId());
-
         return flashcard;
+    }
+
+    public List<Flashcard> getAllFlashcards() {
+        List<Flashcard> allFlashcards = flashcardDAO.getAllFlashcards();
+        return allFlashcards;
+    }
+
+    public int createFlashcard(Flashcard flashcard) {
+        return flashcardDAO.createFlashcard(flashcard);
+    }
+
+    public int updateFlashcard(Flashcard flashcard) {
+        return flashcardDAO.updateFlashcard(flashcard);
+    }
+
+    public int deleteFlashcard(UUID flashcardId) {
+        return flashcardDAO.deleteFlashcard(flashcardId);
     }
 }
