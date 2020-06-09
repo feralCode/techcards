315ef6ce576bfba0f33ef0e06b57b43b0135a7ee
Will Rowston <willrowston@gmail.com>
2020-06-09 10:41:18 -0400

    changed deck subject object to Subject

--- src/main/java/com/techcards/TechCardsBackend/models/dao/decks/DeckMapper.java
+++ src/main/java/com/techcards/TechCardsBackend/models/dao/decks/DeckMapper.java
@@ -2,6 +2,7 @@ package com.techcards.TechCardsBackend.models.dao.decks;
 
 import com.techcards.TechCardsBackend.models.dao.flashcards.Flashcard;
 
+import com.techcards.TechCardsBackend.models.dao.subjects.Subject;
 import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,7 +16,7 @@ public class DeckMapper implements RowMapper<Deck> {
         Deck deck = new Deck();
         deck.setId(resultSet.getObject("deck_id", UUID.class));
         deck.setCreator(resultSet.getString("deck_creator"));
-        deck.setSubject(resultSet.getString("deck_subject"));
+        deck.setSubject((Subject) resultSet.getObject("deck_subject"));
         deck.setFlashcards((Set<Flashcard>) resultSet.getObject("deck_flashcards"));
         deck.setLikes(resultSet.getInt("deck_likes"));
 
