78819e37e4e085ed3c423d22716e63e7081fa082
Will Rowston <willrowston@gmail.com>
2020-06-09 13:25:06 -0400

    added REST routes to subject controller

--- src/main/java/com/techcards/TechCardsBackend/controllers/SubjectController.java
+++ src/main/java/com/techcards/TechCardsBackend/controllers/SubjectController.java
@@ -1,12 +1,12 @@
 package com.techcards.TechCardsBackend.controllers;
 
 import com.techcards.TechCardsBackend.models.dao.subjects.Subject;
+import com.techcards.TechCardsBackend.models.dao.subjects.Subject;
 import com.techcards.TechCardsBackend.models.services.SubjectService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,5 +22,37 @@ public class SubjectController {
     public Subject getSubject(@PathVariable("subjectId") UUID subjectId) {
         Subject subject = subjectService.getSubjectById(subjectId);
         return subject;
+    }
+
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Subject> getAllSubjects() {
+        List<Subject> allSubjects = subjectService.getAllSubjects();
+        return allSubjects;
+    }
+
+    @RequestMapping(value = "/newSubject", method = RequestMethod.POST)
+    public String createSubject(@RequestBody Subject subject) {
+        subjectService.createSubject(subject);
+        return "redirect:/all";
+    }
+
+    @RequestMapping(value="/editSubject/{subjectId}")
+    public String editSubject(@PathVariable("subjectId") UUID subjectId, Model m) {
+        Subject subject = subjectService.getSubjectById(subjectId);
+        m.addAttribute("command", subject);
+        return "editSubjectForm";
+    }
+
+    @RequestMapping(value = "/editSubject", method = RequestMethod.PUT)
+    public String updateSubject(@ModelAttribute("subject") Subject subject) {
+        subjectService.updateSubject(subject);
+        return "redirect:/all";
+    }
+
+    @RequestMapping(value="/deleteSubject/{subjectId}",method = RequestMethod.DELETE)
+    public String deleteSubject(@PathVariable("subjectId") UUID subjectId) {
+        subjectService.deleteSubject(subjectId);
+        return "redirect:/all";
     }
 }
