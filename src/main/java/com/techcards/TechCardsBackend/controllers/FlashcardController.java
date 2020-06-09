0f4f525dd99441e4390a3ac166f7cf4a5829aea3
Will Rowston <willrowston@gmail.com>
2020-06-09 13:33:04 -0400

    completed REST routes for flashcard controller

--- src/main/java/com/techcards/TechCardsBackend/controllers/FlashcardController.java
+++ src/main/java/com/techcards/TechCardsBackend/controllers/FlashcardController.java
@@ -1,12 +1,12 @@
 package com.techcards.TechCardsBackend.controllers;
 
 import com.techcards.TechCardsBackend.models.dao.flashcards.Flashcard;
+import com.techcards.TechCardsBackend.models.dao.flashcards.Flashcard;
 import com.techcards.TechCardsBackend.models.services.FlashcardService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -25,5 +25,36 @@ public class FlashcardController {
         Flashcard flashcard = flashcardService.getFlashcardById(flashcardId);
 
         return flashcard;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Flashcard> getAllFlashcards() {
+        List<Flashcard> allFlashcards = flashcardService.getAllFlashcards();
+        return allFlashcards;
+    }
+
+    @RequestMapping(value = "/newFlashcard", method = RequestMethod.POST)
+    public String createFlashcard(@RequestBody Flashcard flashcard) {
+        flashcardService.createFlashcard(flashcard);
+        return "redirect:/all";
+    }
+
+    @RequestMapping(value="/editFlashcard/{flashcardId}")
+    public String editFlashcard(@PathVariable("flashcardId") UUID flashcardId, Model m) {
+        Flashcard flashcard = flashcardService.getFlashcardById(flashcardId);
+        m.addAttribute("command", flashcard);
+        return "editFlashcardForm";
+    }
+
+    @RequestMapping(value = "/editFlashcard", method = RequestMethod.PUT)
+    public String updateFlashcard(@ModelAttribute("flashcard") Flashcard flashcard) {
+        flashcardService.updateFlashcard(flashcard);
+        return "redirect:/all";
+    }
+
+    @RequestMapping(value="/deleteFlashcard/{flashcardId}",method = RequestMethod.DELETE)
+    public String deleteFlashcard(@PathVariable("flashcardId") UUID flashcardId) {
+        flashcardService.deleteFlashcard(flashcardId);
+        return "redirect:/all";
     }
 }
