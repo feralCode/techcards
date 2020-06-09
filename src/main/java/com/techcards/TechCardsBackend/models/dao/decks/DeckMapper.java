ad9540c68daeac3e996ca7e61ce943f5904af185
Will Rowston <willrowston@gmail.com>
2020-06-09 16:31:07 -0400

    fixed bugs in deck mapper

--- src/main/java/com/techcards/TechCardsBackend/models/dao/decks/DeckMapper.java
+++ src/main/java/com/techcards/TechCardsBackend/models/dao/decks/DeckMapper.java
@@ -2,11 +2,11 @@ package com.techcards.TechCardsBackend.models.dao.decks;
 
 import com.techcards.TechCardsBackend.models.dao.flashcards.Flashcard;
 
-import com.techcards.TechCardsBackend.models.dao.subjects.Subject;
+import com.techcards.TechCardsBackend.models.dao.users.User;
 import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Set;
+import java.util.List;
 import java.util.UUID;
 
 public class DeckMapper implements RowMapper<Deck> {
@@ -15,9 +15,9 @@ public class DeckMapper implements RowMapper<Deck> {
 
         Deck deck = new Deck();
         deck.setId(resultSet.getObject("deck_id", UUID.class));
-        deck.setCreator(resultSet.getString("deck_creator"));
-        deck.setSubject((Subject) resultSet.getObject("deck_subject"));
-        deck.setFlashcards((Set<Flashcard>) resultSet.getObject("deck_flashcards"));
+        deck.setCreator((User) resultSet.getObject("deck_creator"));
+        deck.setSubjectId((UUID) resultSet.getObject("deck_subject_id"));
+        deck.setFlashcards((List<Flashcard>) resultSet.getObject("deck_flashcards"));
         deck.setLikes(resultSet.getInt("deck_likes"));
 
         return deck;
