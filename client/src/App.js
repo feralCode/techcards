4e75fa0916ad7d4b08e212471f0085ad2dc3f405
Will Rowston <willrowston@gmail.com>
2020-06-10 17:12:11 -0400

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Router>
+          <nav className="nav-bar">
+            <div className="logo">TECHCARDS</div>
+            <div className="links">
+              <div className="browse-link">Browse</div>
+              <div className="create-link">Create</div>
+            </div>
+            <div className="user-dropdown">
+              <DropdownButton id="dropdown-basic-button" title="Select User">
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
+            </div>
+          </nav>
+        </Router>
+      </div>
   );
 }
 
