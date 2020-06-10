ce3428782eb6410900e5ebb86f784e548543a221
Will Rowston <willrowston@gmail.com>
2020-06-10 17:52:26 -0400

    added home component and styled nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Home from './components/Home.js'
 
 function App() {
   return (
       <div>
         <Router>
           <nav className="nav-bar">
-            <div className="logo">TECHCARDS</div>
             <div className="links">
-              <div className="browse-link">Browse</div>
-              <div className="create-link">Create</div>
+              <div className="nav-link">Browse</div>
+              <div className="nav-link">Create</div>
+            </div>
+            <div>
+              <div className="logo">TECHCARDS</div>
             </div>
             <div className="user-dropdown">
               <DropdownButton id="dropdown-basic-button" title="Select User">
@@ -24,6 +26,12 @@ function App() {
               </DropdownButton>
             </div>
           </nav>
+
+          <div className="wrapper">
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
         </Router>
       </div>
   );
---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div className="home-page">
+                <h1>Home</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
