fec5d94c4dd0fa5215d57a68c94c33d87860328c
Will Rowston <willrowston@gmail.com>
2020-06-11 11:43:48 -0400

    styled and gave functionality to all subjects component

--- client/src/components/subject/AllSubjects.js
+++ client/src/components/subject/AllSubjects.js
@@ -1,11 +1,47 @@
 import React, {Component} from 'react';
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class AllSubjects extends Component {
+
+    state = {
+        allSubjects: []
+    }
+
+    componentDidMount() {
+        this.getAllSubjects()
+    }
+
+    getAllSubjects = async () => {
+        try {
+            const res = await axios.get('/api/v1/subject/all')
+            const newState = {...this.state}
+            newState.allSubjects = res.data
+            this.setState(newState)
+        } catch (e) {
+            console.log("failed to get allsubjects")
+            console.error(e)
+        }
+    }
+
     render() {
         return (
             <div className="all-subjects">
-                Subjects
+                <div className="title">Select a subject to begin studying</div>
+                <div className="subject-list">
+                    {this.state.allSubjects.map((subject, index) => {
+                        return (
+                            <div key={`t3426gfw7 - ${index}`}>
+                                <div>
+                                    <Link to={`/subject/${subject.id}`}>
+                                        <img className="subject-img" src={subject.imageUrl}/>
+                                        <div className="subject-name">{subject.name}</div>
+                                    </Link>
+                                </div>
+                            </div>
+                            )
+                    })}
+                </div>
             </div>
         );
     }
